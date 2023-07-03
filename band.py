378cd0f5e9314148cbee45991a79f70f0dff1034
emakhebat <malifafif789@gmail.com>
2023-07-04 00:20:08 +0800

    second commit

---
+++ band.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+print("What's name of your grew up in ?\n")
+city_name=input()
+print("What's your pet name ?\n")
+pet_name=input()
+print("Your band name could be " +  city_name + " " + pet_name)
